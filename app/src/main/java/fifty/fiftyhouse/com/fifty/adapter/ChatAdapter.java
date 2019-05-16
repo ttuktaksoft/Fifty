169d4c6528ce4a78ee16f687c21de5ebf3e39174
kdh3040 <kdh30400@naver.com>
2019-05-16 15:55:18 +0900

    [김도형] 채팅리스트 레이아웃 추가

--- app/src/main/java/fifty/fiftyhouse/com/fifty/adapter/ChatAdapter.java
+++ app/src/main/java/fifty/fiftyhouse/com/fifty/adapter/ChatAdapter.java
@@ -5,7 +5,12 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+
+import fifty.fiftyhouse.com.fifty.CommonFunc;
 import fifty.fiftyhouse.com.fifty.R;
 import fifty.fiftyhouse.com.fifty.viewHolder.ChatListHolder;
 
@@ -19,7 +24,7 @@ public class ChatAdapter extends RecyclerView.Adapter<ChatListHolder> {
     public ChatListHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(mContext).inflate(R.layout.fragment_chat_adapter, parent, false);
 
-        //view.setLayoutParams(new_img RelativeLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,mUIData.getHeight()/7));
+        view.setLayoutParams(new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,CommonFunc.getInstance().GetHeightByDevice()/7));
         return new ChatListHolder(view);
     }
 
@@ -29,11 +34,18 @@ public class ChatAdapter extends RecyclerView.Adapter<ChatListHolder> {
     public void onBindViewHolder(ChatListHolder holder, final int position) {
         int i = position;
 
+        Glide.with(mContext)
+                //.load(mMyData.arrSendDataList.get(position).strTargetImg)
+                .load(R.drawable.login_icon)
+                .circleCrop()
+                .diskCacheStrategy(DiskCacheStrategy.ALL)
+                .thumbnail(0.1f)
+                .into(holder.imageView);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return 10;
         //return mMyData.arrChatTargetData.size();
     }
 
