9a906ae83846bf195383dab1a9894e26e3f24a41
DESKTOP-EV9BEG1\Mangomilik <mangomilk114@gmail.com>
2019-06-05 15:05:26 +0900

    [배환웅] 유저페이지 추가

---
+++ app/src/main/java/fifty/fiftyhouse/com/fifty/adapter/UserProfilePhotoAdapter.java
@@ -0,0 +1,45 @@
+package fifty.fiftyhouse.com.fifty.adapter;
+
+import android.content.Context;
+import android.support.constraint.ConstraintLayout;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.bumptech.glide.Glide;
+
+import fifty.fiftyhouse.com.fifty.CommonFunc;
+import fifty.fiftyhouse.com.fifty.R;
+import fifty.fiftyhouse.com.fifty.viewHolder.UserProfilePhotoListHolder;
+
+public class UserProfilePhotoAdapter extends RecyclerView.Adapter<UserProfilePhotoListHolder> {
+
+    Context mContext;
+    public UserProfilePhotoAdapter(Context context) {
+        mContext = context;
+    }
+
+    @Override
+    public UserProfilePhotoListHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(mContext).inflate(R.layout.adapter_user_profile_photo, parent, false);
+
+        return new UserProfilePhotoListHolder(view);
+    }
+
+
+
+    @Override
+    public void onBindViewHolder(UserProfilePhotoListHolder holder, final int position) {
+        int i = position;
+
+        holder.setData(i);
+    }
+
+    @Override
+    public int getItemCount() {
+        return 8;
+        //return mMyData.arrChatTargetData.size();
+    }
+
+}
---
+++ app/src/main/java/fifty/fiftyhouse/com/fifty/viewHolder/UserProfilePhotoListHolder.java
@@ -0,0 +1,40 @@
+package fifty.fiftyhouse.com.fifty.viewHolder;
+
+import android.content.Context;
+import android.media.Image;
+import android.support.constraint.ConstraintLayout;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+
+import fifty.fiftyhouse.com.fifty.CommonFunc;
+import fifty.fiftyhouse.com.fifty.R;
+
+public class UserProfilePhotoListHolder extends RecyclerView.ViewHolder {
+
+    ImageView iv_UserProfile_Photo;
+    Context mContext;
+
+    public UserProfilePhotoListHolder(View itemView) {
+        super(itemView);
+        mContext = itemView.getContext();
+
+        iv_UserProfile_Photo = itemView.findViewById(R.id.iv_UserProfile_Photo);
+
+        int viewCount = 4;
+        int thumbnailSize = CommonFunc.getInstance().getWidthByDevice() / viewCount - CommonFunc.getInstance().convertPXtoDP(mContext.getResources(), 2);
+        ConstraintLayout.LayoutParams lp_Photo = new ConstraintLayout.LayoutParams(thumbnailSize, thumbnailSize);
+        iv_UserProfile_Photo.setLayoutParams(lp_Photo);
+    }
+
+    public void setData(int i)
+    {
+        // TODO 비어 있을때
+        Glide.with(mContext).load(R.drawable.bg_empty_square)
+                .centerCrop()
+                .into(iv_UserProfile_Photo);
+    }
+}
