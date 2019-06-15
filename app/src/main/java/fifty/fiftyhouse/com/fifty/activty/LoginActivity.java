e776ea2c04f6672ef8ebefd3f7d6ff3b31bdff46
kdh3040 <kdh30400@naver.com>
2019-06-15 20:54:38 +0900

    [김도형] 카카오톡 작업중

--- app/src/main/java/fifty/fiftyhouse/com/fifty/activty/LoginActivity.java
+++ app/src/main/java/fifty/fiftyhouse/com/fifty/activty/LoginActivity.java
@@ -7,17 +7,24 @@ import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 
+import com.kakao.auth.AccessTokenCallback;
 import com.kakao.auth.AuthType;
 import com.kakao.auth.ISessionCallback;
 import com.kakao.auth.Session;
+import com.kakao.auth.authorization.accesstoken.AccessToken;
 import com.kakao.network.ErrorResult;
 import com.kakao.usermgmt.LoginButton;
 import com.kakao.usermgmt.UserManagement;
 import com.kakao.usermgmt.callback.MeResponseCallback;
+import com.kakao.usermgmt.callback.MeV2ResponseCallback;
+import com.kakao.usermgmt.response.MeV2Response;
 import com.kakao.usermgmt.response.model.UserProfile;
 import com.kakao.util.exception.KakaoException;
+import com.kakao.util.helper.log.Logger;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import fifty.fiftyhouse.com.fifty.CommonFunc;
@@ -32,16 +39,8 @@ public class LoginActivity extends AppCompatActivity {
     private LoginButton btn_kakao_login;
 
 
-    private static LoginActivity _Instance;
     private SessionCallback callback;
 
-    public static LoginActivity getInstance() {
-        if (_Instance == null)
-        {
-            _Instance = new LoginActivity();
-        }
-        return _Instance;
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,6 +52,7 @@ public class LoginActivity extends AppCompatActivity {
         iv_kakao_login = findViewById(R.id.iv_kakao_login);
         btn_kakao_login = (LoginButton) findViewById(R.id.btn_kakao_login);
 
+        callback= new SessionCallback();
 
         iv_kakao_login.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -60,14 +60,9 @@ public class LoginActivity extends AppCompatActivity {
                 // TODO 로그인 처리
                 FirebaseManager.getInstance().SignInAnonymously(LoginActivity.this);
                 Session session = Session.getCurrentSession();
-
-                session.addCallback(new SessionCallback());
-
+                session.addCallback(callback);
                 session.open(AuthType.KAKAO_LOGIN_ALL, LoginActivity.this);
 
-                //startActivity(new Intent(getApplicationContext(),SignUpActivity.class));
-                //startActivity(new Intent(getApplicationContext(),MainActivity.class));
-                //finish();
             }
         });
     }
@@ -92,7 +87,6 @@ public class LoginActivity extends AppCompatActivity {
         // 로그인에 성공한 상태
 
         @Override
-
         public void onSessionOpened() {
             requestMe();
         }
@@ -107,57 +101,33 @@ public class LoginActivity extends AppCompatActivity {
 
         public void requestMe() {
 
-            final Map<String, String> properties = new HashMap<String, String>();
-            properties.put("nickname", "leo");
-            properties.put("age", "33");
+            List<String> keys = new ArrayList<>();
+            keys.add("properties.nickname");
+            keys.add("properties.profile_image");
+            keys.add("kakao_account.email");
+            keys.add("kakao_account.gender");
+            keys.add("kakao_account.age_range");
 
-            UserManagement.getInstance().requestMe(new MeResponseCallback() {
-
+            UserManagement.getInstance().me(keys, new MeV2ResponseCallback() {
                 @Override
-                public void onSuccess(UserProfile result) {
-                    Log.e("SessionCallback :: ", "onSuccess");
-
-
-                    String nickname = result.getNickname();
-
-                    String email = result.getEmail();
-
-                    String profileImagePath = result.getProfileImagePath();
-
-                    String thumnailPath = result.getThumbnailImagePath();
-
-                    String UUID = result.getUUID();
-
-
-                    long id = result.getId();
-
-
-                    Log.e("Profile : ", nickname + "");
-
-                    Log.e("Profile : ", email + "");
-
-                    Log.e("Profile : ", profileImagePath  + "");
-
-                    Log.e("Profile : ", thumnailPath + "");
-
-                    Log.e("Profile : ", UUID + "");
-
-                    Log.e("Profile : ", id + "");
-
-                    redirectSignupActivity();
+                public void onFailure(ErrorResult errorResult) {
+                    String message = "failed to get user info. msg=" + errorResult;
+                    Logger.d(message);
                 }
 
                 @Override
-
                 public void onSessionClosed(ErrorResult errorResult) {
-
-                    Log.e("SessionCallback :: ", "onSessionClosed : " + errorResult.getErrorMessage());
+                    Logger.d("onSessionClosed");
                 }
 
-                // 회원이 아닌 경우,
                 @Override
-                public void onNotSignedUp() {
-                    Log.e("SessionCallback :: ", "onNotSignedUp");
+                public void onSuccess(MeV2Response response) {
+                    Logger.d("user id : " + response.getId());
+                    Logger.d("email: " + response.getKakaoAccount().getEmail());
+                    Logger.d("email: " + response.getKakaoAccount().getAgeRange());
+                    Logger.d("email: " + response.getKakaoAccount().getGender());
+                    //Logger.d("profile image: " + response.getKakaoAccount().getProfileImagePath());
+                    MoveMainActivity();
                 }
             });
 
@@ -165,6 +135,12 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     protected void redirectSignupActivity() {
+        final Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    protected void MoveMainActivity() {
         final Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
         finish();
