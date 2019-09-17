e8508d42f973e1f83ea4e7aeee274ed505c56ebf
DESKTOP-EV9BEG1\Mangomilik <mangomilk114@gmail.com>
2019-09-17 17:26:33 +0900

    [배환웅] 메인화면 추천 탭 작업

---
+++ app/src/main/java/fifty/fiftyhouse/com/fifty/adapter/RealTimeFavoriteAdapter.java
@@ -0,0 +1,76 @@
+package fifty.fiftyhouse.com.fifty.adapter;
+
+import android.content.Context;
+import android.content.res.ColorStateList;
+import android.util.TypedValue;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import androidx.core.content.ContextCompat;
+import androidx.core.widget.ImageViewCompat;
+import androidx.recyclerview.widget.RecyclerView;
+import fifty.fiftyhouse.com.fifty.CommonData;
+import fifty.fiftyhouse.com.fifty.R;
+
+public class RealTimeFavoriteAdapter extends RecyclerView.Adapter<RealTimeFavoriteHolder> {
+
+    Context mContext;
+    int mItemCount = 0;
+    ArrayList<String> mItemList = new ArrayList<>();
+
+    public RealTimeFavoriteAdapter(Context context) {
+        mContext = context;
+    }
+
+    @Override
+    public RealTimeFavoriteHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(mContext).inflate(R.layout.adapter_realtime_favorite, parent, false);
+
+        return new RealTimeFavoriteHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(RealTimeFavoriteHolder holder, final int position) {
+        int i = position;
+        holder.setData(mItemList.get(i), i + 1);
+    }
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+    @Override
+    public int getItemCount() {
+        return mItemCount;
+    }
+
+    public void setItemData(ArrayList<String> list)
+    {
+        mItemList.clear();
+        mItemList.addAll(list);
+
+        mItemCount = mItemList.size();
+    }
+}
+
+class RealTimeFavoriteHolder extends RecyclerView.ViewHolder {
+
+    TextView tv_RealTime_Favorite;
+    Context mContext;
+
+    public RealTimeFavoriteHolder(View itemView) {
+        super(itemView);
+        mContext = itemView.getContext();
+
+        tv_RealTime_Favorite  = itemView.findViewById(R.id.tv_RealTime_Favorite);
+    }
+
+    public void setData(String favorite, int count)
+    {
+        tv_RealTime_Favorite.setText(count + "위 : " + favorite);
+    }
+}
--- app/src/main/java/fifty/fiftyhouse/com/fifty/viewPager/MainTodayViewPager.java
+++ app/src/main/java/fifty/fiftyhouse/com/fifty/viewPager/MainTodayViewPager.java
@@ -1,5 +1,6 @@
 package fifty.fiftyhouse.com.fifty.viewPager;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.felipecsl.asymmetricgridview.library.widget.AsymmetricGridView;
@@ -18,21 +19,26 @@ import android.widget.TextView;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
 import fifty.fiftyhouse.com.fifty.CommonData;
 import fifty.fiftyhouse.com.fifty.CommonFunc;
+import fifty.fiftyhouse.com.fifty.DataBase.ClubData;
 import fifty.fiftyhouse.com.fifty.DialogFunc;
 import fifty.fiftyhouse.com.fifty.MainActivity;
 import fifty.fiftyhouse.com.fifty.Manager.FirebaseManager;
 import fifty.fiftyhouse.com.fifty.Manager.TKManager;
 import fifty.fiftyhouse.com.fifty.R;
+import fifty.fiftyhouse.com.fifty.activty.ClubActivity;
+import fifty.fiftyhouse.com.fifty.activty.ClubListActivity;
 import fifty.fiftyhouse.com.fifty.adapter.CustomGridListHolder;
 import fifty.fiftyhouse.com.fifty.adapter.CustomMainAdapterOne;
 import fifty.fiftyhouse.com.fifty.adapter.FavoriteViewAdapter;
 import fifty.fiftyhouse.com.fifty.adapter.MainAdapterOne;
+import fifty.fiftyhouse.com.fifty.adapter.RealTimeFavoriteAdapter;
 import fifty.fiftyhouse.com.fifty.util.OnRecyclerItemClickListener;
 import fifty.fiftyhouse.com.fifty.util.OnSingleClickListener;
 import fifty.fiftyhouse.com.fifty.util.RecyclerItemClickListener;
@@ -42,15 +48,17 @@ public class MainTodayViewPager extends Fragment {
     TextView tv_Main_Today_UserList_Empty, tv_Main_Today_Desc;
     FloatingActionButton fa_Main_Today_Search;
     RecyclerView rv_Main_Today_Favorite;
+    RecyclerView rv_RealTime_Favorite;
+    TextView tv_RealTime_Favorite_Time;
     AsymmetricGridView rv_Main_Today_UserList;
     View v_FragmentView = null;
     public CustomMainAdapterOne mAdapter;
     ArrayList<String> mUserList = new ArrayList<>();
-    FavoriteViewAdapter mFavoriteViewAdapter;
+    RealTimeFavoriteAdapter mRealTimeFavoriteAdapter;
     private String UserIndex;
 
-    ArrayList<String> mFavoriteViewList = new ArrayList<>();
-    String mSelectFavoriteKey = "";
+    ArrayList<String> mRealTimeFavoriteViewList = new ArrayList<>();
+    String mSelectRealTimeFavorite = "";
 
     public MainTodayViewPager() {
         super();
@@ -64,46 +72,24 @@ public class MainTodayViewPager extends Fragment {
             v_FragmentView = inflater.inflate(R.layout.viewpager_main_today, container, false);
 
             tv_Main_Today_UserList_Empty = v_FragmentView.findViewById(R.id.tv_Main_Today_UserList_Empty);
+            rv_RealTime_Favorite = v_FragmentView.findViewById(R.id.rv_RealTime_Favorite);
+            tv_RealTime_Favorite_Time = v_FragmentView.findViewById(R.id.tv_RealTime_Favorite_Time);
             rv_Main_Today_UserList = v_FragmentView.findViewById(R.id.rv_Main_Today_UserList);
-            rv_Main_Today_Favorite = v_FragmentView.findViewById(R.id.rv_Main_Today_Favorite);
             fa_Main_Today_Search = v_FragmentView.findViewById(R.id.fa_Main_Today_Search);
             tv_Main_Today_Desc = v_FragmentView.findViewById(R.id.tv_Main_Today_Desc);
 
+            Set EntrySet = TKManager.getInstance().MyData.GetUserFavoriteListKeySet();
+            List array = new ArrayList(EntrySet);
+            mSelectRealTimeFavorite = array.get(0).toString();
+
             initRecyclerView();
             initSubInfo();
-            initFavoriteRecyclerView();
+            initRealTimeFavoriteRecyclerView();
 
             fa_Main_Today_Search.setOnClickListener(new OnSingleClickListener() {
                 @Override
                 public void onSingleClick(View view) {
-                    //DialogFunc.getInstance().ShowUserSearchPopup(getContext(), MainActivity.mActivity);
-
-                    DialogFunc.getInstance().ShowLoadingPage(MainActivity.mActivity);
-
-                    FirebaseManager.CheckFirebaseComplete listener = new FirebaseManager.CheckFirebaseComplete() {
-
-                        @Override
-                        public void CompleteListener() {
-                            DialogFunc.getInstance().DismissLoadingPage();
-                            RefreshAdapter();
-
-                        }
-
-                        @Override
-                        public void CompleteListener_Yes() {
-
-                        }
-
-                        @Override
-                        public void CompleteListener_No() {
-
-                        }
-                    };
-
-                    Set EntrySet = TKManager.getInstance().MyData.GetUserFavoriteListKeySet();
-                    List array = new ArrayList(EntrySet);
-                    FirebaseManager.getInstance().FindFavoriteList(array.get(1).toString(), listener);
-
+                    DialogFunc.getInstance().ShowUserSearchPopup(getContext(), MainActivity.mActivity);
                 }
             });
         }
@@ -117,29 +103,11 @@ public class MainTodayViewPager extends Fragment {
 
     private void initSubInfo()
     {
-        Set EntrySet = TKManager.getInstance().MyData.GetUserFavoriteListKeySet();
-        List array = new ArrayList(EntrySet);
-        tv_Main_Today_Desc.setText(CommonFunc.getInstance().getComleteWordByJongsung(array.get(0).toString(), "을", "를") + " " + CommonFunc.getInstance().getStr(getResources(), R.string.MSG_MAIN_USER_TODAY_DESC));
+        tv_Main_Today_Desc.setText(CommonFunc.getInstance().getComleteWordByJongsung(mSelectRealTimeFavorite, "을", "를") + " " + CommonFunc.getInstance().getStr(getResources(), R.string.MSG_MAIN_USER_TODAY_DESC));
     }
 
     private void initRecyclerView()
     {
-        /*mAdapter = new MainAdapterOne(getContext());
-        RefreshAdapter();
-        mAdapter.setHasStableIds(true);
-        mAdapter.SetItemCountByType(CommonData.MainViewType.HOT, TKManager.getInstance().View_UserList_Hot.size());
-
-        rv_Main_Today_UserList.setAdapter(mAdapter);
-        rv_Main_Today_UserList.setLayoutManager(new GridLayoutManager(getContext(), 3));
-        rv_Main_Today_UserList.addOnItemTouchListener(new RecyclerItemClickListener(getContext(), rv_Main_Today_UserList, new OnRecyclerItemClickListener() {
-            @Override
-            public void onSingleClick(View view, int position) {
-
-                UserIndex = TKManager.getInstance().UserData_Simple.get(TKManager.getInstance().View_UserList_Hot.get(position)).GetUserIndex();
-                CommonFunc.getInstance().GetUserDataInFireBase(UserIndex, MainActivity.mActivity, false);
-            }
-        }));*/
-
         RefreshUserList();
         mAdapter =  new CustomMainAdapterOne(getContext(), CommonFunc.getInstance().getCustomGridListHolderList(mUserList));
         mAdapter.SetItemCountByType(CommonData.MainViewType.HOT);
@@ -180,74 +148,57 @@ public class MainTodayViewPager extends Fragment {
         }
     }
 
-    private void initFavoriteRecyclerView()
+    private void initRealTimeFavoriteRecyclerView()
     {
-        mFavoriteViewAdapter = new FavoriteViewAdapter(getContext());
-        RefreshFavoriteViewListSlot();
-        mFavoriteViewAdapter.setFontSize(CommonData.FAVORITE_FONT_SIZE_SMALL);
-        mFavoriteViewAdapter.setHasStableIds(true);
-
-        rv_Main_Today_Favorite.setAdapter(mFavoriteViewAdapter);
-        rv_Main_Today_Favorite.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false));
-        rv_Main_Today_Favorite.addOnItemTouchListener(new RecyclerItemClickListener(getContext(), rv_Main_Today_Favorite, new OnRecyclerItemClickListener() {
+        mRealTimeFavoriteAdapter = new RealTimeFavoriteAdapter(getContext());
+        RefreshRealTimeFavoriteList();
+        mRealTimeFavoriteAdapter.setHasStableIds(true);
+        rv_RealTime_Favorite.setAdapter(mRealTimeFavoriteAdapter);
+        rv_RealTime_Favorite.setLayoutManager(new LinearLayoutManager(getContext()));
+        rv_RealTime_Favorite.addOnItemTouchListener(new RecyclerItemClickListener(getContext(), rv_RealTime_Favorite, new OnRecyclerItemClickListener() {
             @Override
-            public void onSingleClick(View view, int position) {
+            public void onSingleClick(View view, final int position) {
 
-                mSelectFavoriteKey = mFavoriteViewList.get(position);
-                RefreshFavoriteViewListSlot();
-                mFavoriteViewAdapter.notifyDataSetChanged();
-/*                UserIndex = TKManager.getInstance().UserData_Simple.get(TKManager.getInstance().View_UserList_Hot.get(position)).GetUserIndex();
-                CommonFunc.getInstance().GetUserDataInFireBase(UserIndex, MainActivity.mActivity, false);*/
+                DialogFunc.getInstance().ShowLoadingPage(MainActivity.mActivity);
+
+                FirebaseManager.CheckFirebaseComplete listener = new FirebaseManager.CheckFirebaseComplete() {
+
+                    @Override
+                    public void CompleteListener() {
+                        DialogFunc.getInstance().DismissLoadingPage();
+                        mSelectRealTimeFavorite = mRealTimeFavoriteViewList.get(position);
+                        initSubInfo();
+                        RefreshAdapter();
+                    }
+
+                    @Override
+                    public void CompleteListener_Yes() {
+
+                    }
+
+                    @Override
+                    public void CompleteListener_No() {
+
+                    }
+                };
+
+                FirebaseManager.getInstance().FindFavoriteList(mRealTimeFavoriteViewList.get(position), listener);
             }
         }));
 
-        rv_Main_Today_Favorite.addOnItemTouchListener(new RecyclerView.OnItemTouchListener() {
+        /*rv_RealTime_Favorite.setOnTouchListener(new View.OnTouchListener() {
             @Override
-            public boolean onInterceptTouchEvent(RecyclerView rv, MotionEvent e) {
-                int action = e.getAction();
-                switch (action) {
-                    case MotionEvent.ACTION_MOVE:
-                        rv.getParent().requestDisallowInterceptTouchEvent(true);
-                        break;
-                    default:
-                        rv.getParent().requestDisallowInterceptTouchEvent(false);
-                        break;
-                }
-                return false;
+            public boolean onTouch(View v, MotionEvent event) {
+                return true;
             }
-
-            @Override
-            public void onTouchEvent(RecyclerView rv, MotionEvent e) {
-
-            }
-
-            @Override
-            public void onRequestDisallowInterceptTouchEvent(boolean disallowIntercept) {
-
-            }
-        });
+        });*/
     }
 
-    private void RefreshFavoriteViewListSlot() {
-        mFavoriteViewList.clear();
-        Iterator<String> it = TKManager.getInstance().MyData.GetUserFavoriteListKeySet().iterator();
-
-        while(it.hasNext())
-        {
-            String key = it.next();
-            mFavoriteViewList.add(key);
-        }
-
-        mFavoriteViewAdapter.setItemCount(TKManager.getInstance().MyData.GetUserFavoriteListCount());
-        mFavoriteViewAdapter.setItemData(mFavoriteViewList);
-        mFavoriteViewAdapter.setSelectView(true);
-
-        if(CommonFunc.getInstance().CheckStringNull(mSelectFavoriteKey) ||
-                mFavoriteViewList.contains(mSelectFavoriteKey) == false)
-            mSelectFavoriteKey = mFavoriteViewList.get(0);
-
-        ArrayList<String> selectlist = new ArrayList<>();
-        selectlist.add(mSelectFavoriteKey);
-        mFavoriteViewAdapter.setSelectItemData(selectlist);
+    public void RefreshRealTimeFavoriteList()
+    {
+        mRealTimeFavoriteViewList.clear();
+        mRealTimeFavoriteViewList.addAll(TKManager.getInstance().SearchList_Favorite);
+        mRealTimeFavoriteAdapter.setItemData(mRealTimeFavoriteViewList);
+        mRealTimeFavoriteAdapter.notifyDataSetChanged();
     }
 }
