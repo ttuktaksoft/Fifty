6f956a3f0c4276b5d8d6f9da77c9e39a8f3d48ec
kdh3040 <kdh30400@naver.com>
2020-03-26 01:16:51 +0900

    [김도형] 파이어베이스 저장소에 업로드

--- public/fireinit.js
+++ public/fireinit.js
@@ -3,12 +3,12 @@
 // TODO: Replace with your project's customized code snippet
 // Required for side-effects
 const firebaseConfig = {
-    apiKey: "AIzaSyBAQLEwLDQuzD54kQG4c4pK7q29YKhzvkY",
-    authDomain: "fifty-e2d17.firebaseapp.com",
-    databaseURL: "https://fifty-e2d17.firebaseio.com",
-    projectId: "fifty-e2d17",
-    storageBucket: "fifty-e2d17.appspot.com",
-    messagingSenderId: "475674577888",
-    appId: "1:475674577888:web:7c2abd7953496959"
-  };
+  apiKey: "AIzaSyDfNWgccbmVowMoJno27WMJykLuOEyDdgw",
+  authDomain: "fifty-3bc03.firebaseapp.com",
+  databaseURL: "https://fifty-3bc03.firebaseio.com",
+  projectId: "fifty-3bc03",
+  storageBucket: "fifty-3bc03.appspot.com",
+  messagingSenderId: "791257379373",
+  appId: "1:791257379373:web:a783526966264c0e65bea4"
+};
 firebase.initializeApp(firebaseConfig);
